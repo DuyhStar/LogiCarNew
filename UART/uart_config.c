0c23f1ae21cefb37eb35ddfc2f7695a1004afe02
DuYihao <b17090713@njupt.edu.cn>
2019-03-18 22:08:07 +0800

    Add uart2 function.

--- UART/uart_config.c
+++ UART/uart_config.c
@@ -4,7 +4,21 @@
  *  Created on: 2019Äê3ÔÂ18ÈÕ
  *      Author: DuYihao
  */
+#include "uart_config.h"
 
 
+void UART2_Init(uint32_t Baud)
+{
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART2);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
+    GPIOPinConfigure(GPIO_PD6_U2RX);
+    GPIOPinConfigure(GPIO_PD7_U2TX);
+    GPIOPinTypeUART(GPIO_PORTD_BASE, GPIO_PIN_6 | GPIO_PIN_7);
 
+    UARTConfigSetExpClk(UART2_BASE, SysCtlClockGet(), Baud, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    UARTIntRegister(UART2_BASE, IntHandler_UART2);
+    UARTIntEnable(UART2_BASE, UART_INT_RX | UART_INT_RT);
+    IntEnable(INT_UART2);
+}
--- UART/uart_config.h
+++ UART/uart_config.h
@@ -8,8 +8,21 @@
 #ifndef UART_UART_CONFIG_H_
 #define UART_UART_CONFIG_H_
 
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/interrupt.h"
+#include "utils/uartstdio.h"
 
 
-
+void UART2_Init(uint32_t Baud);
+void IntHandler_UART2(void);
 
 #endif /* UART_UART_CONFIG_H_ */
