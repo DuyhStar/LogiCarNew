19a7cdc4d47dee3deebe287cce251525ccf8482b
DuYihao <b17090713@njupt.edu.cn>
2019-03-18 22:07:37 +0800

    14 black detect function.

--- Sensor/patrol_line.c
+++ Sensor/patrol_line.c
@@ -29,3 +29,240 @@ void patrol_line_init()
     GPIOPadConfigSet(GPIO_PORTE_BASE, E_pin, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 }
 
+//wideÎªÏß¿í,È¡0,1,2
+void forward_patrol_line(uint8_t wide)
+{
+    extern int forward_speed, turn_speed;
+    int8_t left = 0, right = 0;
+
+    switch(wide)
+    {
+    case 0:
+        left  = f2_black();
+        right = f4_black();
+        break;
+    case 1:
+        left  = f1_black();
+        right = f5_black();
+        break;
+    case 2:
+        left  = f0_black();
+        right = f6_black();
+        break;
+    default:
+        break;
+    }
+
+    if( left && right )
+        car_forward(forward_speed);
+    else if(right)
+        car_turn_right(turn_speed);
+    else if(left)
+        car_turn_left(turn_speed);
+    else
+        car_forward(forward_speed);
+}
+
+
+void back_patrol_line(uint8_t wide)
+{
+    extern int forward_speed, turn_speed;
+    int8_t left = 0, right = 0;
+
+    switch(wide)
+    {
+    case 0:
+        left  = b2_black();
+        right = b4_black();
+        break;
+    case 1:
+        left  = b1_black();
+        right = b5_black();
+        break;
+    case 2:
+        left  = b0_black();
+        right = b6_black();
+        break;
+    default:
+        break;
+    }
+
+    if( left && right )
+        car_back(forward_speed);
+    else if(right)
+        car_turn_right(turn_speed);
+    else if(left)
+        car_turn_left(turn_speed);
+    else
+        car_back(forward_speed);
+}
+
+int8_t f0_black()
+{
+    uint32_t port = GPIO_PORTA_BASE;
+    uint32_t pin  = GPIO_PIN_2;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t f1_black()
+{
+    uint32_t port = GPIO_PORTA_BASE;
+    uint32_t pin  = GPIO_PIN_3;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t f2_black()
+{
+    uint32_t port = GPIO_PORTA_BASE;
+    uint32_t pin  = GPIO_PIN_4;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t f3_black()
+{
+    uint32_t port = GPIO_PORTA_BASE;
+    uint32_t pin  = GPIO_PIN_5;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t f4_black()
+{
+    uint32_t port = GPIO_PORTB_BASE;
+    uint32_t pin  = GPIO_PIN_4;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t f5_black()
+{
+    uint32_t port = GPIO_PORTB_BASE;
+    uint32_t pin  = GPIO_PIN_5;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t f6_black()
+{
+    uint32_t port = GPIO_PORTD_BASE;
+    uint32_t pin  = GPIO_PIN_2;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+
+int8_t m_black()
+{
+    uint32_t port = GPIO_PORTD_BASE;
+    uint32_t pin  = GPIO_PIN_3;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+
+int8_t b0_black()
+{
+    uint32_t port = GPIO_PORTE_BASE;
+    uint32_t pin  = GPIO_PIN_4;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t b1_black()
+{
+    uint32_t port = GPIO_PORTE_BASE;
+    uint32_t pin  = GPIO_PIN_3;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t b2_black()
+{
+    uint32_t port = GPIO_PORTE_BASE;
+    uint32_t pin  = GPIO_PIN_2;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t b3_black()
+{
+    uint32_t port = GPIO_PORTE_BASE;
+    uint32_t pin  = GPIO_PIN_1;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t b4_black()
+{
+    uint32_t port = GPIO_PORTE_BASE;
+    uint32_t pin  = GPIO_PIN_0;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t b5_black()
+{
+    uint32_t port = GPIO_PORTD_BASE;
+    uint32_t pin  = GPIO_PIN_5;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+int8_t b6_black()
+{
+    uint32_t port = GPIO_PORTD_BASE;
+    uint32_t pin  = GPIO_PIN_4;
+
+    int32_t s = GPIOPinRead(port, pin);
+    if( (s&pin) ==  pin)
+        return 1;
+    else
+        return 0;
+}
+
+
