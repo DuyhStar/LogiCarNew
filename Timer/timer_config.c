a2a8833d475c7765c227ac2204878c47a395d94e
DuYihao <b17090713@njupt.edu.cn>
2019-03-18 22:07:52 +0800

    System tick timer init.

--- Timer/timer_config.c
+++ Timer/timer_config.c
@@ -4,7 +4,16 @@
  *  Created on: 2019Äê3ÔÂ18ÈÕ
  *      Author: DuYihao
  */
+#include "timer_config.h"
 
+void SysTick_Init_ms(uint8_t ui8IntTime)
+{
+    SysTickPeriodSet((SysCtlClockGet()/1000)*ui8IntTime);
 
-
+    SysTickIntRegister(IntHandler_SysTick);
+    SysTickIntEnable();
+    //SysTickEnable();
+    SysTickDisable();
+    IntMasterDisable();
+}
 
--- Timer/timer_config.h
+++ Timer/timer_config.h
@@ -8,8 +8,14 @@
 #ifndef TIMER_TIMER_CONFIG_H_
 #define TIMER_TIMER_CONFIG_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/systick.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
 
-
+void SysTick_Init_ms(uint8_t ui8IntTime);
+void IntHandler_SysTick(void);
 
 
 #endif /* TIMER_TIMER_CONFIG_H_ */
