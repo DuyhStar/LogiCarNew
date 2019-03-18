e66ecc90323c35900f21baf3ee528139173c496c
DuYihao <b17090713@njupt.edu.cn>
2019-03-18 22:07:11 +0800

    Key function.

--- GPIO/gpio_config.c
+++ GPIO/gpio_config.c
@@ -4,7 +4,34 @@
  *  Created on: 2019Äê3ÔÂ18ÈÕ
  *      Author: DuYihao
  */
+#include "gpio_config.h"
 
+void key1_init()
+{
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+}
 
+uint8_t Key1_Read()
+{
+    uint8_t s;
+    s = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4);
 
+    if(s != GPIO_PIN_4)
+        return 1;
+    else
+        return 0;
+}
 
+void system_waitKey()
+{
+    while(1)
+    {
+        if(Key1_Read() == 1)
+        {
+            delay_ms(20);
+            if(Key1_Read() == 1)
+                break;
+        }
+    }
+}
--- GPIO/gpio_config.h
+++ GPIO/gpio_config.h
@@ -8,8 +8,17 @@
 #ifndef GPIO_GPIO_CONFIG_H_
 #define GPIO_GPIO_CONFIG_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "delay.h"
 
-
+void    key1_init();
+uint8_t Key1_Read();
+void    system_waitKey();
 
 
 #endif /* GPIO_GPIO_CONFIG_H_ */
